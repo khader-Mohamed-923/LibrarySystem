c1e06543df3654db7950f81745bb9294d173e31b
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-09 00:11:56 +0300

    feat: implement service layer business logic and domain exceptions

---
+++ src/LibrarySystem.Services/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using LibrarySystem.Services.Implementations;
+
+namespace LibrarySystem.Services;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<BookService>();
+
+        return services;
+    }
+}
