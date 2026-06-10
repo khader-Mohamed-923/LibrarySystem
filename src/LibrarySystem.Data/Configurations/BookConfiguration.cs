2553c1157e0465fcac1ac3cf18bc0bd982263d89
Khader Mohamed <khaderelhabal923@gmail.com>
2026-06-10 16:21:36 +0300

    refactor(book): add partial update support and improve API response

--- src/LibrarySystem.Data/Configurations/BookConfiguration.cs
+++ src/LibrarySystem.Data/Configurations/BookConfiguration.cs
@@ -4,7 +4,6 @@ using LibrarySystem.Data.Entities;
 
 namespace LibrarySystem.Data.Configurations;
 
-
 public class BookConfiguration : IEntityTypeConfiguration<Book>
 {
     public void Configure(EntityTypeBuilder<Book> builder)
@@ -13,41 +12,27 @@ public class BookConfiguration : IEntityTypeConfiguration<Book>
             .IsRequired()
             .HasMaxLength(200);
 
-      
         builder.Property(b => b.Author)
             .IsRequired()
             .HasMaxLength(150);
 
-     
         builder.Property(b => b.ISBN)
             .IsRequired()
             .IsFixedLength()
             .HasMaxLength(13);
 
-      
         builder.HasIndex(b => b.ISBN)
             .HasDatabaseName("UX_Books_ISBN")
             .IsUnique();
 
-     
-        builder.Property(b => b.TotalCopies)
-            .IsRequired();
 
-   
-        builder.Property(b => b.AvailableCopies)
-            .IsRequired();
-
-      
         builder.Property(b => b.RowVersion)
-            .IsRowVersion()
-            .IsConcurrencyToken();
+            .IsRowVersion();
 
-   
         builder.ToTable(b => b.HasCheckConstraint(
             "CK_Books_TotalCopies",
             "[TotalCopies] >= 1"));
 
-      
         builder.ToTable(b => b.HasCheckConstraint(
             "CK_Books_AvailableCopies",
             "[AvailableCopies] >= 0"));
@@ -56,7 +41,6 @@ public class BookConfiguration : IEntityTypeConfiguration<Book>
             .HasDatabaseName("IX_Books_AvailableCopies")
             .HasFilter("[AvailableCopies] > 0");
 
-      
         builder.HasMany(b => b.Loans)
             .WithOne(l => l.Book)
             .HasForeignKey(l => l.BookId)
